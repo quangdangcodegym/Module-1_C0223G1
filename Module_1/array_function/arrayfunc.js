600a26ac0ac27e4ddb3f1ea8dd81281d27abcdee
quangdangcodegym <quang.dang@codegym.vn>
2023-03-22 09:58:50 +0700

    sua bai tap tu dien

---
+++ Module_1/array_function/arrayfunc.js
@@ -0,0 +1,28 @@
+let arr = [5,2,7,8];
+
+// arr.push(9);         // thêm 1 phần tử vào cuối mảng
+// console.log(arr);
+
+// arr.push(9,10,11);
+// console.log(arr);
+
+// arr.pop();              // xóa 1 phần tử cuối mảng
+// console.log(arr);
+
+
+// arr.shift();           // Xóa 1 phần tử đầu mảng
+// console.log(arr);
+
+
+// arr.unshift(4)          // Thêm 1 hoặc nhiều phần tử vào đầu mảng
+// console.log(arr);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+fruits2.splice(2, 2);
+
+
